591fdbb542d786b61e369c4585baf414ee492b06
Helton Barbosa <helton1000xd8@gmail.com>
2024-05-01 18:13:38 -0300

    Create the particles section and start the hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
+import Hero from "@/Components/Hero";
+import MobileNav from "@/Components/MobileNav";
 import Nav from "@/Components/Nav";
-import React from "react";
+import React, { useState } from "react";
 
 const PaginaInicial = () => {
-  return <div className="overflow-x-hidden">
-    <div>
-      <Nav/>
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        {/* animação */}
+        <Hero />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default PaginaInicial;
