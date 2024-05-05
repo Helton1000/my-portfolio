c19830d10981c21c87388486e2c7320ccb04e7c6
Helton Barbosa <helton1000xd8@gmail.com>
2024-05-05 16:07:20 -0300

    updated description in the projects section

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -13,6 +13,14 @@ const Projects = () => {
 
       <ProjectCard
         title="Projeto 1"
+        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque a
+        ipsam quidem quis hic beatae explicabo, laborum esse nulla ratione
+        suscipit numquam voluptate dicta ipsum porro, corporis recusandae!
+        Harum, aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Impedit similique, voluptate facilis nostrum consequuntur
+        molestias assumenda reprehenderit quasi numquam! Voluptatum voluptate
+        at libero distinctio illum, voluptatem reiciendis cupiditate
+        voluptates nihil?"
         tech="React"
         tech2="Next Js"
         tech3="Tailwind"
@@ -21,6 +29,14 @@ const Projects = () => {
       />
       <ProjectCard
         title="Projeto 2"
+        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque a
+        ipsam quidem quis hic beatae explicabo, laborum esse nulla ratione
+        suscipit numquam voluptate dicta ipsum porro, corporis recusandae!
+        Harum, aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Impedit similique, voluptate facilis nostrum consequuntur
+        molestias assumenda reprehenderit quasi numquam! Voluptatum voluptate
+        at libero distinctio illum, voluptatem reiciendis cupiditate
+        voluptates nihil?"
         tech="React"
         tech2="Next Js"
         tech3="Tailwind"
@@ -29,6 +45,14 @@ const Projects = () => {
       />
       <ProjectCard
         title="Projeto 3"
+        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque a
+        ipsam quidem quis hic beatae explicabo, laborum esse nulla ratione
+        suscipit numquam voluptate dicta ipsum porro, corporis recusandae!
+        Harum, aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Impedit similique, voluptate facilis nostrum consequuntur
+        molestias assumenda reprehenderit quasi numquam! Voluptatum voluptate
+        at libero distinctio illum, voluptatem reiciendis cupiditate
+        voluptates nihil?"
         tech="React"
         tech2="Next Js"
         tech3="Tailwind"
@@ -37,6 +61,14 @@ const Projects = () => {
       />
       <ProjectCard
         title="Projeto 4"
+        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque a
+        ipsam quidem quis hic beatae explicabo, laborum esse nulla ratione
+        suscipit numquam voluptate dicta ipsum porro, corporis recusandae!
+        Harum, aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Impedit similique, voluptate facilis nostrum consequuntur
+        molestias assumenda reprehenderit quasi numquam! Voluptatum voluptate
+        at libero distinctio illum, voluptatem reiciendis cupiditate
+        voluptates nihil? Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dicta perspiciatis nihil, ab reiciendis laudantium nam fugit temporibus, repellat error architecto! Tenetur modi facere possimus laudantium ipsum perferendis dolorum facilis!"
         tech="React"
         tech2="Next Js"
         tech3="Tailwind"
