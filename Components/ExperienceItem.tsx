c2ef317c97812d9ef448ac5b51c62bbca7da42a3
Helton Barbosa <helton1000xd8@gmail.com>
2024-05-05 14:58:51 -0300

    upgrade experience section

--- Components/ExperienceItem.tsx
+++ Components/ExperienceItem.tsx
@@ -7,28 +7,17 @@ interface Props {
   description: string;
 }
 
-const ExperienceItem = ({
-  title,
-  year,
-  Button = false,
-  description,
-}: Props) => {
+const ExperienceItem = ({ title, year, Button = false, description }: Props) => {
   return (
     <div className="mb-[4rem] md:mb-[8rem]">
       <span className="px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55e6a5]">
         {year}
       </span>
-      <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">
-        {title}
-      </h1>
-      <p className="text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80">
-        {description}
-      </p>
-      {Button && (
-        <a className="relative flex h-[50px] w-40 items-center justify-center rounded-2xl font-bold overflow-hidden bg-[#55e6a5] text-black uppercase shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-yellow-400 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56 mt-[1rem]">
-          <span className="relative z-10">Learn More</span>
-        </a>
-      )}
+      <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">{title}</h1>
+      <p className="text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae voluptas totam cupiditate distinctio, eveniet, error pariatur inventore dicta officia repellat alias, repudiandae tenetur. Incidunt iusto nemo amet at odit.</p>
+      <a className={`relative flex h-[50px] w-40 items-center justify-center rounded-2xl font-bold overflow-hidden bg-[#55e6a5] text-black uppercase shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-yellow-400 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56 mt-[1rem] ${Button ? '' : 'invisible'}`}>
+        <span className="relative z-10">Learn More</span>
+      </a>
     </div>
   );
 };
